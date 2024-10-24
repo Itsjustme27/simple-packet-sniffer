d134ffd0412f3f483121325d843e5ded57cad550
Itsjustme27 <reachprayush@gmail.com>
2024-10-24 18:05:17 +0545

    fixes and added input()

--- sniffer.py
+++ sniffer.py
@@ -1,7 +1,7 @@
 import socket
 import os
 
-HOST_NAME = '192.168.1.64'
+HOST = input("Enter your IP: ")
 
 def main():
     if os.name == 'nt':
@@ -12,7 +12,7 @@ def main():
     sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
     sniffer.bind((HOST, 0))
 
-    sniffer.setsockopt(socket.IPPROTO_IP, socket.IPHDRINCL, 1)
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
     if os.name == 'nt':
         sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
@@ -23,7 +23,8 @@ def main():
     if os.name == 'nt':
         sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
 
-    if __name__ == '__main__':
+
+if __name__ == '__main__':
         main()
 
 
